cb3a7f0b887d8236d85c79b6686dce87a6808d1c
Jarek <jarek.krych@agreatergrid.com>
2026-07-08 12:45:42 -0600

    Initial code commit. Created an initial microservice packages and added entry point to the Spring Boot application.

---
+++ src/main/java/com/bluestaq/note_taking_service/NoteTakingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bluestaq.note_taking_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteTakingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteTakingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bluestaq/note_taking_service/NoteTakingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bluestaq.note_taking_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteTakingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
